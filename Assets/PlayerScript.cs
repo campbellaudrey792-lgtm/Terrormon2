c6838b5146fb137ae8588c9e16d3fd65f4f0f336
campbellaudrey792-lgtm <campbellaudrey792@gmail.com>
2025-11-11 10:15:08 -0500

    a

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -1,16 +1,67 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem; // NEW input system
 
-public class PlayerScript : MonoBehaviour
+[RequireComponent(typeof(PlayerInput))]
+public class PlayerScript_NewInput : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Tuning")]
+    public float moveSpeed = 5f;
+    public float stepSize = 1f;
+
+    [Header("State")]
+    public bool isMoving;
+
+    private PlayerInput playerInput;
+    private InputAction moveAction;
+
+    private void Awake()
     {
-        
+        playerInput = GetComponent<PlayerInput>();
+        // Make sure your action is literally named "Move" in the asset
+        moveAction = playerInput.actions["Move"];
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable() => moveAction.Enable();
+    private void OnDisable() => moveAction.Disable();
+
+    private void Update()
     {
-        
+        if (isMoving) return;
+
+        Vector2 input = moveAction.ReadValue<Vector2>();
+
+        // Convert analog to grid step (-1/0/1)
+        input.x = Mathf.Sign(input.x);
+        input.y = Mathf.Sign(input.y);
+
+        // Optional: block diagonals
+        if (Mathf.Abs(input.x) > Mathf.Abs(input.y)) input.y = 0f;
+        else input.x = 0f;
+
+        if (input != Vector2.zero)
+        {
+            var targetPos = transform.position;
+            targetPos.x += input.x * stepSize;
+            targetPos.y += input.y * stepSize;
+
+            StartCoroutine(Move(targetPos));
+        }
+    }
+
+    private IEnumerator Move(Vector3 targetPos)
+    {
+        isMoving = true;
+        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(
+                transform.position,
+                targetPos,
+                moveSpeed * Time.deltaTime
+            );
+            yield return null;
+        }
+        transform.position = targetPos;
+        isMoving = false;
     }
 }
